2f4b54e80b7a5dec1f8eb14b8d05107079043aad
Rajbir Singh <rajbirsingh1233@gmail.com>
2021-09-08 18:25:47 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () =>{
+  return(
+    <>
+      <div className="main_div">
+      <div className="center_div">
+        <h1>Login</h1>
+        <br />
+
+        <p>See your growth and get consulting support!</p>
+
+        <br /><br />
+        <a href="https://accounts.google.com/" target="_blank"><button id="signin_google"> 
+                <i class="fab fa-google"></i>
+                <span id="google_text">&nbsp;&nbsp; Sign in with Google</span>
+        </button></a>
+        <br /><br />
+
+        <fieldset>
+          <legend>&nbsp;&nbsp;or Sign in with Email&nbsp;&nbsp;</legend>
+        </fieldset>
+
+        <br />
+        <div>
+          <form action="submit">
+
+          <label>Email*</label>
+          <br />
+          <input type="email" id="form" placeholder="mail@website.com" required/>
+          <br /><br />
+          <label>Password*</label>
+          <br />
+          <input type="password" id="form" minlength="8" placeholder="Min.8 character" required/>
+          
+          <br /><br />
+
+          <input type="checkbox" id="checked"/> 
+          <label>&nbsp;&nbsp;Remeber me</label>
+          <a href="#"><span id="forgot">Forget password?</span></a>
+          <button id="login_button" type="submit" value="Submit" >Login</button>
+
+          </form>
+
+          <p id="register">Not registered yet? <a href="#"><span>Create an Account</span></a></p>
+
+        </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
